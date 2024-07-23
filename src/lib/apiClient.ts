f5a1497bcc8c4d243f39d160f51d43aa51b544d9
A-Yuki28 <yukiaminaka28@gmail.com>
2024-07-23 20:47:21 +0900

    [add]src/components/ProfilesEdit.tsxを追加。プロフィール編集を行う機能の追加するため。[update]src/profile/[userId].tsxを修正

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const apiClient = axios.create({
   //localhostで立ち上げるときはこのURL
-  // baseURL: "http://localhost:8000/api",
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: "http://localhost:10000/api",
+  //baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
